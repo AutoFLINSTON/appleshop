f2f45812f9faeb2345ce8598968c3418af52f08f
Vadim Klychev <vadimslim@mail.ru>
2015-08-18 08:13:13 +0300

    Доделал footer

--- app/js/sub_menu.js
+++ app/js/sub_menu.js
@@ -17,3 +17,33 @@ function openMenu(id){
 	}
 }
 window.onload=startList;
+
+
+window.onload = function() { 
+
+	var scrollUp = document.getElementById('toTop'); 
+
+	scrollUp.onmouseover = function() { 
+		scrollUp.style.opacity=0.5;
+		scrollUp.style.filter  = 'alpha(opacity=50)';
+	};
+
+	scrollUp.onmouseout = function() { 
+		scrollUp.style.opacity = 0.3;
+		scrollUp.style.filter  = 'alpha(opacity=30)';
+	};
+
+	scrollUp.onclick = function() {
+		window.scrollTo(0,0);
+	};
+
+
+
+	window.onscroll = function () { 
+		if ( window.pageYOffset > 0 ) {
+			scrollUp.style.display = 'block';
+		} else {
+			scrollUp.style.display = 'none';
+		}
+	};
+};
